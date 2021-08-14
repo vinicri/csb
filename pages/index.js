012e00f5711ccdb6be32b98cde8da45b8df536d4
Vinicius Silva <5026706+vinicri@users.noreply.github.com>
2021-08-14 11:27:43 -0300

    add getTodos request

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,40 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getTodos } from "../api";
 
 export default function IndexPage() {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setError(null);
+    try {
+      const response = await getTodos();
+      setTodos(response.data);
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  if (error) {
+    return (
+      <div>
+        <p>Oops! Error getting todos list.</p>
+        <button onClick={getData}>Try again</button>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
-        <a>About</a>
+        <a>Aut</a>
       </Link>
     </div>
-  )
+  );
 }

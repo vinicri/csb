3207cbc369dee24348e21deb7f1bf82e3ce0dcca
Vinicius Silva <5026706+vinicri@users.noreply.github.com>
2021-08-14 14:44:43 -0300

    add table styles

--- components/TodosTable.js
+++ components/TodosTable.js
@@ -7,7 +7,12 @@ export default function TodosTable({ todos }) {
         <th>completed</th>
       </tr>
       {todos.map((todo, index) => (
-        <tr key={todo.id} className={`${index % 2 === 0 ? "bg-gray-200" : ""}`}>
+        <tr
+          key={todo.id}
+          className={`${
+            index % 2 === 0 ? "bg-gray-200" : ""
+          } hover:bg-gray-300 cursor-pointer`}
+        >
           <td>{todo.userId}</td>
           <td>{todo.title}</td>
           <td>{todo.completed.toString()}</td>

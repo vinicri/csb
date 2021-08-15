fc64dd6a4f31e77e77735f63de7c167b4c26470d
Vinicius Silva <5026706+vinicri@users.noreply.github.com>
2021-08-15 20:31:26 -0300

    few tests

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,3 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import IndexPage from "./index";
+import React from "react";
+
+jest.mock("axios");
+
 describe("test todo list page", () => {
-  it("test", () => {});
+  it("renders with get todos button", () => {
+    render(<IndexPage />);
+    screen.getByRole("button", { name: "Get todos" });
+  });
+
+  it("renders with get todos button", () => {
+    render(<IndexPage />);
+    const button = screen.getByRole("button", { name: "Get todos" });
+    userEvent.click(button);
+  });
 });
